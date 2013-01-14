a4605db5ee6af7354f18bac4661cab62b8c656c9
Shreyas <shreyas19881988@gmail.com>
2013-01-14 22:47:24 +0530

    init

---
+++ equality_detector.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+// equality_detector.v
+// Assignment_2 
+// Question : 3
+// Name : Rupapara Prashant Govindlal
+// Roll Num : 11307R008
+
+module equality_detector (zero_out,gtz_out,result_in);
+parameter BUS_WIDTH = 32;
+
+output zero_out,gtz_out;
+input  [BUS_WIDTH-1 : 0]result_in; 	
+
+wire  zero_out,gtz_out;
+wire  [BUS_WIDTH-1 : 0] result_in; 
+
+assign zero_out = result_in ? 0 : 1;
+assign gtz_out = result_in[BUS_WIDTH-1];
+		
+endmodule
